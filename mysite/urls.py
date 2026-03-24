93c367fc77326d7f0600e6d10575cf8a98f23483
Dmitriy <dmitriy@Dmitriys-MacBook-Air.local>
2026-03-24 18:15:14 +0500

    photo fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map_points.urls')),
+    path('map/', include('map_points.urls')),
 ]
 
 if settings.DEBUG:
