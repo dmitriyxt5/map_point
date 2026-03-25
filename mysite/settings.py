1d1c94eb7d6a2124c8c12e08d85807242efcf4df
Dmitriy <dmitriy@Dmitriys-MacBook-Air.local>
2026-03-25 18:44:52 +0500

    filesystem is changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles' # Папка, куда WhiteNoise будет смотреть
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+AWS_LOCATION = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
