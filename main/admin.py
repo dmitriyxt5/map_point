7bfa710ba57a7590d479516fa8686307b33abea3
Dmitriy <dmitriy@Dmitriys-MacBook-Air.local>
2026-03-25 21:48:02 +0500

    map viewer

--- main/admin.py
+++ main/admin.py
@@ -1,23 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin  # для стандартных полей
+from django.contrib.auth.admin import UserAdmin
+from .models import MapPoint
 
-admin.site.unregister(User)
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
 
-class DemoAdmin(UserAdmin): 
+class DemoUserAdmin(UserAdmin):
     def has_delete_permission(self, request, obj=None):
-        if request.user.username == "demo":
-            return False
-        return super().has_delete_permission(request, obj)
-
+        return False if request.user.username == "demo" else super().has_delete_permission(request, obj)
     def has_add_permission(self, request):
-        if request.user.username == "demo":
-            return False
-        return super().has_add_permission(request, obj)
-
+        return False if request.user.username == "demo" else super().has_add_permission(request, obj)
     def has_change_permission(self, request, obj=None):
-        if request.user.username == "demo":
-            return True 
-        return super().has_change_permission(request, obj)
+        return True if request.user.username == "demo" else super().has_change_permission(request, obj)
 
-admin.site.register(User, DemoAdmin)
+admin.site.register(User, DemoUserAdmin)
+
+class DemoAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False if request.user.username == "demo" else super().has_delete_permission(request, obj)
+    def has_add_permission(self, request):
+        return False if request.user.username == "demo" else super().has_add_permission(request, obj)
+    def has_change_permission(self, request, obj=None):
+        return True if request.user.username == "demo" else super().has_change_permission(request, obj)
+
+admin.site.register(MapPoint, DemoAdmin)
